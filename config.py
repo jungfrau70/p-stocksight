ac5f7addba74e49c738f81f2c6cc6b59061cc144
InHwan Jung <inhwan.jung@gmail.com>
2021-06-14 17:04:20 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,21 @@
+elasticsearch_host = "elasticsearch"
+elasticsearch_port = 9200
+elasticsearch_user = "admin"
+elasticsearch_password = "4team123!"
+# bearer_token='AAAAAAAAAAAAAAAAAAAAAIlCQgEAAAAAL3zZGbS798Hrpxu%2B6PTCAhD%2FCRw%3D1dY9bdBVtKmzs9mEskWSMjiU3CC2jJ10rOWHMb6F0HPZ8x98ND'
+consumer_key = "UytwbSwanw8xkzfaTRiRp2qVo"
+consumer_secret = "DnDxgCIq5OWTreeJuZmhlru2nxta4Jl9Ky8VLs5MakVk9lVyZA"
+access_token = "104431837-3qUEKOgfzz44NjBbPhXUrLG4FzQk0UA1Sssip7N5"
+access_token_secret = "LvUOXGJcNmzFJlmT11dyKxTGukDP2TiXWCh0v3eIr5PCN"
+nltk_tokens_required = ("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink")
+nltk_min_tokens = 1
+nltk_tokens_ignored = ("win", "giveaway")
+twitter_feeds = ["@elonmusk", "@cnbc", "@benzinga", "@stockwits",
+                 "@Newsweek", "@WashingtonPost", "@breakoutstocks", "@bespokeinvest",
+                 "@WSJMarkets", "@stephanie_link", "@nytimesbusiness", "@IBDinvestors",
+                 "@WSJDealJournal", "@jimcramer", "@TheStalwart", "@TruthGundlach",
+                 "@Carl_C_Icahn", "@ReformedBroker", "@bespokeinvest", "@stlouisfed",
+                 "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
+                 "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
+                 "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
+                 "@BuzzFeed","@nytimes"]
